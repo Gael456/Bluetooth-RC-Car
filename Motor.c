f274c2eb4987092cd50e6807e56db41202300240
Gael456 <143972983+Gael456@users.noreply.github.com>
2025-01-31 17:46:54 -0800

    Motor Driver

---
+++ Motor.c
@@ -0,0 +1,111 @@
+/**
+ * @file Stepper_Motor.c
+ *
+ * @brief Source file for the Stepper_Motor driver.
+ *
+ * This file contains the function definitions for the Stepper_Motor driver. It uses
+ * GPIO pins to provide output signals to the ULN2003 stepper motor driver.
+ * 
+ * The following components are used:
+ *	-	3.3V - 6V TT Motor
+ *  - ULN2003 Stepper Motor Driver
+ *  - 3.3V / 5V Breadboard Power Supply Module (External Power Source)
+ *
+ * @author Gael Esparza Lobatos
+ */
+
+#include "Motor.h"
+ 
+void Motor_Init(uint16_t period_constant, uint16_t duty_cycle)
+{
+	// Return from the function if the specified duty_cycle is greater than
+	// or equal to the given period. The duty cycle cannot exceed 99%
+	if (duty_cycle >= period_constant) return;
+	
+	// Enable the clock to PWM module 0 by setting th R0 bit
+	// in the RCGCPWM register
+	SYSCTL->RCGCPWM |= 0x01;
+	
+	// Enable the clock to Port B by setting the
+	// R1 bit (Bit 1) in the RCGCPIO register
+	SYSCTL->RCGCGPIO |= 0x02;
+	
+	// Configure the PB4, PB5, PB6, PB7 pins as output
+	// by setting Bits 3 to 0 in the DIR register
+	GPIOB->DIR |= 0xF0;
+	
+	// Configure the PB4, PB5, PB6, PB7 pins to function as
+	// PWM output pins by enabling alternate functions in the AFSEL register
+	GPIOB->AFSEL |= 0xF0;
+	
+	// Clear an set the PCTL register PB4, PB5, PB6, PB7
+	GPIOB->PCTL &= ~0xFFFF0000;
+	GPIOB->PCTL |= 0x44440000;
+	
+	// Enable the digital functionality for the PB4, PB5, PB6, and PB7 pins
+	// by setting Bits 3 to 0 in the DEN register
+	GPIOB->DEN |= 0xF0;
+	
+	// Disable PWM0 Generator during configuration
+	PWM0->_0_CTL &= ~0x01;
+	PWM0->_1_CTL &= ~0x01;
+	
+	// Set the ACTcmPAD field (Bits 7 to 6) to 0x3 in the PWM0GENA register
+	// to drive the PWM signal high when the counter matches
+	// the comparator (i.e. the value in PWM0CMPA) while counting down
+	PWM0->_0_GENA |= 0x0000008C;
+	
+	// Set the preriod by writing to the LOAD field (bits 3 to 2) to 0x2 in the PWM0GENA register
+	// to drive the PWM signal low when the counter matches the value
+	// in the PWM0LOAD register
+	PWM0->_0_GENB |= 0x0000080C;
+	
+	// Set the period by writing to the LOAD field (Bits 15 to 0)
+	// in the PWM0LOAD register. The determines the number of clock
+	// cycles needed to count down to zero
+	PWM0->_0_LOAD = (period_constant - 1);
+	
+	// Set the duty cycle by writing to the COMPA field (Bits 1 to 0)
+	// in the PWM0CMPA register. When the counter matches the value in this register,
+	// the PWM signal will be driven high
+	PWM0->_0_CMPA = (duty_cycle - 1);
+	PWM0->_0_CMPB = (duty_cycle - 1);
+	
+	// Set the ACTcmPAD field (Bits 7 to 6) to 0x3 in the PWM1GENA register
+	// to drive the PWM signal high when the counter matches
+	// the comparator (i.e. the value in PWM0CMPA) while counting down
+	PWM0->_1_GENA |= 0x0000008C;
+	
+	// Set the preriod by writing to the LOAD field (bits 3 to 2) to 0x2 in the PWM0GENA register
+	// to drive the PWM signal low when the counter matches the value
+	// in the PWM0LOAD register
+	PWM0->_1_GENB |= 0x0000080C;
+	
+	// Set the period by writing to the LOAD field (Bits 15 to 0)
+	// in the PWM0LOAD register. The determines the number of clock
+	// cycles needed to count down to zero
+	PWM0->_1_LOAD = (period_constant - 1);
+	
+	// Set the duty cycle by writing to the COMPA field (Bits 1 to 0)
+	// in the PWM0CMPA register. When the counter matches the value in this register,
+	// the PWM signal will be driven high
+	PWM0->_1_CMPA = (duty_cycle);
+	PWM0->_1_CMPB = (duty_cycle);
+	
+	// Enable the PWM0_0 block after configuration by setting the
+	// ENABLE bit (Bit 0) in the PWM0CTL register
+	PWM0->_0_CTL |= 0x01;
+	PWM0->_1_CTL |= 0x01;
+	
+	// Enable the PWM0_0 block to be passed to the PB4, PB5, PB6, and PB7 pin (M0PWM0-M0PWM3)
+	PWM0->ENABLE |= 0x0F;
+	
+}
+
+void PWM0_0_Update_Duty_Cycle(uint16_t duty_cycle)
+{
+	// Set the duty cycle by writing to the COMPA field (Bits 15 to 0)
+	// in the PW0CMPA register. When the counter matches the value in this register,
+	// the PWM signal will be driven high
+	PWM0->_0_CMPA = duty_cycle;
+}
---
+++ Motor.h
@@ -0,0 +1,37 @@
+/**
+ * @file Motor.h
+ *
+ * @brief Header file for the Motor driver.
+ *
+ * This file contains the function definitions for the Stepper_Motor driver. It uses
+ * GPIO pins to provide output signals to the ULN2003 stepper motor driver.
+ * 
+ * The following components are used:
+ *	-	3V-6V TT Motor
+ *  - ULN2003 Stepper Motor Driver
+ *  - 3.3V / 5V Breadboard Power Supply Module (External Power Source)
+ *
+ * @author Gael Esparza Lobatos
+ */
+
+#include "TM4C123GH6PM.h"
+
+/**
+ * @brief Initializes GPIO pins required for controlling the stepper motor.
+ *
+ * @param Configure the necessary GPIO pins as outputsand sets them up as digital
+ * functionality to control the stepper motor through the connected motor driver.
+ *
+ * @return None
+ */
+void Motor_Init(uint16_t period_constant, uint16_t duty_cycle);
+
+
+/**
+ * @brief 
+ *
+ * @param 
+ *
+ * @return None
+ */
+void PWM0_0_Update_Duty_Cycle(uint16_t duty_cycle);
